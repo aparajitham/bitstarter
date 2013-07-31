b2b185d2ff8d8b0cd30dfe1655e050066204733f
aparajitham <jeetu.vii@gmail.com>
2013-07-31 11:41:47 +0000

    fixed the variable thingy for express

--- web.js
+++ web.js
@@ -1,7 +1,8 @@
 
 var express = require('express');
+var newapp = express();
 var fs = require('fs');
-var app = express.createServer(express.logger());
+var app = newapp.createServer(newapp.logger());
 var buf = new Buffer(30);
 buf = (fs.readFileSync('index.html'));
 app.get('/', function(request, response) {
